0bed2639c25869d35f4625a93bdcd786348e82eb
jaekim3220 <jehyunkim9@gmail.com>
2023-12-18 17:15:21 +0900

    1차 맵 생성 완료

--- game.py
+++ game.py
@@ -29,13 +29,29 @@ main_right_wall = Entity(model='cube', position=(5, 5, 5), color=color.white,
                          )
 '''
 맵을 만들기 위한 노가다 시작
+-> EditorCamera()를 사용하며 확인하는 것이 편함
 '''
 
+# 앞쪽
+front_floor = Entity(model='cube', position=(-25, 0, 55), 
+                     scale=(60, 1, 10), color=color.light_gray, 
+                     collider='box')
+front_ceiling = Entity(model='cube', position=(-25, 10, 55), 
+                       scale=(60, 1, 10), color=color.light_gray, 
+                       collider='box')
+front_left_wall = Entity(model='cube', position=(-30, 5, 50), 
+                         scale=(50, 10, 1), collider='box', 
+                         texture='pic/wall.jpg', texture_scale=(16, 10))
+front_right_wall = Entity(model='cube', position=(-20, 5, 60), 
+                          scale=(50, 10, 1), collider='box', 
+                          texture='pic/wall.jpg', texture_scale=(16, 10))
 
-# EditorCamera()  # 맵 생성 중 카메라 구도 조정(마우스 우클릭)
-character = FirstPersonController() # 1인칭으로 맵 보기
-character.cursor.visible = False    # 커서 보이기 방지
-character.gravity = 0.5 # 중력에 따라 캐릭터 점프력 차이
-character.speed = 10    # 이동 속도
+
+EditorCamera()  # 맵 생성 중 카메라 구도 조정(마우스 우클릭)
+
+# character = FirstPersonController() # 1인칭으로 맵 보기
+# character.cursor.visible = False    # 커서 보이기 방지
+# character.gravity = 0.5 # 중력에 따라 캐릭터 점프력 차이
+# character.speed = 10    # 이동 속도
 
 app. run()
